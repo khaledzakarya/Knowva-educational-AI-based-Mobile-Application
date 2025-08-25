1819d3eabd8a41a33744c2a53351292555f7eb94
AboEl3iz <k.hassan202077@gmail.com>
2025-08-25 16:54:30 +0300

    install dependancies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,20 +11,20 @@ export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   eslintPluginPrettierRecommended,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-      ecmaVersion: 5,
-      sourceType: 'module',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-  },
+  // {
+  //   languageOptions: {
+  //     globals: {
+  //       ...globals.node,
+  //       ...globals.jest,
+  //     },
+  //     ecmaVersion: 5,
+  //     sourceType: 'module',
+  //     parserOptions: {
+  //       projectService: true,
+  //       tsconfigRootDir: import.meta.dirname,
+  //     },
+  //   },
+  // },
   {
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
