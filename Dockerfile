552ec690b369b40c5bff2e2fe6a190f080812354
AboEl3iz <k.hassan202077@gmail.com>
2025-08-30 19:39:13 +0300

    test for flutter

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,6 @@ COPY package*.json ./
 RUN npm install
 COPY . .
 EXPOSE 3000
-CMD ["npm", "run", "start:dev"]
+RUN npm run build
+RUN npx prisma generate
+CMD ["node", "dist/main.js"]
--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
     forbidNonWhitelisted: true,
     transform: true,
   }));
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
 
